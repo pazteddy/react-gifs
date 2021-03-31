9497328ec3152430bbb4cf51bc9a247c97ea7450
Teddy Paz <pazteddy@gmail.com>
2021-03-31 16:46:08 -0400

    se cambio la direccion a htpps

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( categoria )=>{
 
-  const url= `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(categoria) }&limit=10&api_key=2rPsp0GvJxUDllDQN4BjIlb8YV9N6VM0`;
+  const url= `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(categoria) }&limit=10&api_key=2rPsp0GvJxUDllDQN4BjIlb8YV9N6VM0`;
   const resp = await fetch(url);
   const {data} = await resp.json();
   //console.log(data);
