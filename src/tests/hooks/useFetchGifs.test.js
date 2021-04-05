1e90cf598c3b0eeec937edbee35d1f9e7d5ecba0
Teddy Paz <pazteddy@gmail.com>
2021-04-05 14:13:27 -0400

    Se hizo test de Hooks

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -2,14 +2,25 @@ import { useFetchGifs } from '../../hooks/useFetchGifs';
 import { renderHook } from '@testing-library/react-hooks';
 
 describe('Pruebas en el hook useFetchGifs', () => {
-  test('Debe de retornar el estado inicial', () => {
+  test('Debe de retornar el estado inicial', async() => {
     // Isntalar la libreria +
-    const { result } = renderHook ( () => useFetchGifs('Heroe Academia'));
+    const { result, waitForNextUpdate } = renderHook ( () => useFetchGifs('sHeroe Academia'));
     const { data, loading } = result.current;
-    // const { data:images,loading } = useFetchGifs('Heroe Academia');
+    await waitForNextUpdate();
     expect( data ).toEqual([]);
     expect( loading ).toBe(true);
+  });
+
+  test('Debe retornar un arreglo de imgs y loading en False', async() => {
+    const { result, waitForNextUpdate } = renderHook ( () => useFetchGifs('Heroe Academia'));
+    await waitForNextUpdate();
+
+    const { data, loading } = result.current;
+    expect( data.length ).toEqual(10);
+    expect( loading ).toBe(false);
+    
   })
+  
   
   
 })
